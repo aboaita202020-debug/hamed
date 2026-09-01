2da6fac3055bac10d905478a8701aaadd9558763
aboaita202020-debug <aboaita202020@gmail.com>
2026-09-01 09:38:34 +0300

    Add integrated Hamed service exports

---
+++ app/services/__init__.py
@@ -0,0 +1,13 @@
+"""Safe business-service engines used by Hamed AI."""
+from .platform import (
+    CRM, EducationCouncil, OpportunityEngine, PackageEngine, OfferEngine,
+    NegotiationEngine, RestaurantGrowthEngine, ReputationEngine, SalesMessageEngine,
+    ServiceCatalog, WebsiteAnalyzer, StoreAnalyzer, QRMenu, KnowledgeBase,
+)
+
+__all__ = [
+    "CRM", "EducationCouncil", "OpportunityEngine", "PackageEngine", "OfferEngine",
+    "NegotiationEngine", "RestaurantGrowthEngine", "ReputationEngine",
+    "SalesMessageEngine", "ServiceCatalog", "WebsiteAnalyzer", "StoreAnalyzer",
+    "QRMenu", "KnowledgeBase",
+]
